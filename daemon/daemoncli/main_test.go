dd9ca5b3c0990977b46ac8358391cb80e056e4f8
Cyril Galibern <cyril.galibern@opensvc.com>
2022-01-15 15:40:57 +0100

    [test] Reduce WaitRunningDelay during checks

--- daemon/daemoncli/main_test.go
+++ daemon/daemoncli/main_test.go
@@ -2,30 +2,19 @@ package daemoncli
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
+
+func init() {
+	WaitRunningDelay = 10 * time.Millisecond
+}
 
 func TestDaemonStartThenStop(t *testing.T) {
 	require.False(t, Running())
 	go func() {
 		require.Nil(t, Start())
-	}()
-	require.Nil(t, WaitRunning())
-	require.True(t, Running())
-	require.Nil(t, Stop())
-	require.False(t, Running())
-}
-
-func TestDaemonStartRestartThenStop(t *testing.T) {
-	require.False(t, Running())
-	go func() {
-		require.Nil(t, Start())
-	}()
-	require.Nil(t, WaitRunning())
-	require.True(t, Running())
-	go func() {
-		require.Nil(t, ReStart())
 	}()
 	require.Nil(t, WaitRunning())
 	require.True(t, Running())
